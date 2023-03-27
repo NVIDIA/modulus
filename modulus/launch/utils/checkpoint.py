36d421d73bce37c0cbd66c28a97336d880950648
Nicholas Geneva <5533524+NickGeneva@users.noreply.github.com>
2023-03-27 16:20:35 -0700

    Checkpoint update (#1)

--- modulus/launch/utils/checkpoint.py
+++ modulus/launch/utils/checkpoint.py
@@ -15,8 +15,9 @@
 import glob
 import re
 import torch
+import modulus
 
-from typing import Union, List, NewType
+from typing import Union, List, NewType, Dict
 from pathlib import Path
 from torch.optim.lr_scheduler import _LRScheduler
 from torch.cuda.amp import GradScaler
@@ -33,7 +34,10 @@ checkpoint_logging = PythonLogger("checkpoint")
 
 
 def _get_checkpoint_filename(
-    path: str, index: Union[int, None] = None, saving: bool = False
+    path: str,
+    base_name: str = "checkpoint",
+    index: Union[int, None] = None,
+    saving: bool = False,
 ) -> str:
     """Gets the file name /path of checkpoint
 
@@ -47,7 +51,9 @@ def _get_checkpoint_filename(
     Parameters
     ----------
     path : str
-         Path to checkpoints
+        Path to checkpoints
+    base_name: str, optional
+        Base file name, by default checkpoint
     index : Union[int, None], optional
         Checkpoint index, by default None
     saving : bool, optional
@@ -59,30 +65,34 @@ def _get_checkpoint_filename(
         Checkpoint file name
     """
     # Get model parallel rank so all processes in the first model parallel group
-    # can save their checkpoint. In the case without model parallelism, model_parallel_rank
-    # should be the same as the process rank itself and only rank 0 saves
+    # can save their checkpoint. In the case without model parallelism,
+    # model_parallel_rank should be the same as the process rank itself and
+    # only rank 0 saves
     manager = DistributedManager()
     model_parallel_rank = (
         manager.group_rank("model_parallel") if manager.distributed else 0
     )
 
     # Input file name
-    checkpoint_filename = str(Path(path) / f"checkpoint.{model_parallel_rank}")
+    checkpoint_filename = str(
+        Path(path).resolve() / f"{base_name}.{model_parallel_rank}"
+    )
     # If epoch is provided load that file
-    if not index is None:
+    if index is not None:
         checkpoint_filename = checkpoint_filename + f".{index}"
-        checkpoint_filename += ".pth"
+        checkpoint_filename += ".pt"
     # Otherwise try loading the latest epoch or rolling checkpoint
     else:
         file_names = []
-        for fname in glob.glob(checkpoint_filename + "*.pth", recursive=False):
+        for fname in glob.glob(checkpoint_filename + "*.pt", recursive=False):
             file_names.append(Path(fname).name)
 
         if len(file_names) > 0:
             # If checkpoint from a null index save exists load that
-            # This is the most likely line to error since it will fail with invalid checkpoint names
+            # This is the most likely line to error since it will fail with
+            # invalid checkpoint names
             file_idx = [
-                int(re.sub(f"^checkpoint.{model_parallel_rank}.|.pth", "", fname))
+                int(re.sub(f"^{base_name}.{model_parallel_rank}.|.pt", "", fname))
                 for fname in file_names
             ]
             file_idx.sort()
@@ -91,16 +101,60 @@ def _get_checkpoint_filename(
                 checkpoint_filename = checkpoint_filename + f".{file_idx[-1]+1}"
             else:
                 checkpoint_filename = checkpoint_filename + f".{file_idx[-1]}"
-            checkpoint_filename += ".pth"
+            checkpoint_filename += ".pt"
         else:
-            checkpoint_filename += ".0.pth"
+            checkpoint_filename += ".0.pt"
 
     return checkpoint_filename
 
 
+def _unique_model_names(
+    models: List[torch.nn.Module],
+) -> Dict[str, torch.nn.Module]:
+    """Util to clean model names and index if repeat names, will also strip DDP wrappers
+    if they exist.
+
+    Parameters
+    ----------
+    model :  List[torch.nn.Module]
+        List of models to generate names for
+
+    Returns
+    -------
+    Dict[str, torch.nn.Module]
+        Dictionary of model names and respective modules
+    """
+    # Loop through provided models and set up base names
+    model_dict = {}
+    for model0 in models:
+        if hasattr(model0, "module"):
+            # Strip out DDP layer
+            model0 = model0.module
+        # Base name of model is meta.name unless pytorch model
+        base_name = model0.__class__.__name__
+        if isinstance(model0, modulus.Module):
+            base_name = model0.meta.name
+        # If we have multiple models of the same name, introduce another index
+        if base_name in model_dict:
+            model_dict[base_name].append(model0)
+        else:
+            model_dict[base_name] = [model0]
+
+    # Set up unique model names if needed
+    output_dict = {}
+    for key, model in model_dict.items():
+        if len(model) > 1:
+            for i, model0 in enumerate(model):
+                output_dict[key + str(i)] = model0
+        else:
+            output_dict[key] = model[0]
+
+    return output_dict
+
+
 def save_checkpoint(
     path: str,
-    model: Union[torch.nn.Module, List[torch.nn.Module], None] = None,
+    models: Union[torch.nn.Module, List[torch.nn.Module], None] = None,
     optimizer: Union[optimizer, None] = None,
     scheduler: Union[scheduler, None] = None,
     scaler: Union[scaler, None] = None,
@@ -109,14 +163,14 @@ def save_checkpoint(
     """Training checkpoint saving utility
 
     This will save a training checkpoint in the provided path following the file naming
-    convention "checkpoint.{model parallel id}.{epoch/index}.pth". The load checkpoint
+    convention "checkpoint.{model parallel id}.{epoch/index}.pt". The load checkpoint
     method in Modulus core can then be used to read this file.
 
     Parameters
     ----------
     path : str
         Path to save the training checkpoint
-    model : Union[torch.nn.Module, List[torch.nn.Module], None], optional
+    models : Union[torch.nn.Module, List[torch.nn.Module], None], optional
         A single or list of PyTorch models, by default None
     optimizer : Union[optimizer, None], optional
         Optimizer, by default None
@@ -126,8 +180,8 @@ def save_checkpoint(
         AMP grad scaler. Will attempt to save on in static capture if none provided, by
         default None
     epoch : Union[int, None], optional
-        Epoch checkpoint to load. If none this will save the checkpoint in the next valid
-        index, by default None
+        Epoch checkpoint to load. If none this will save the checkpoint in the next
+        valid index, by default None
     """
     # Create checkpoint directory if it does not exist
     if not Path(path).is_dir():
@@ -136,18 +190,23 @@ def save_checkpoint(
         )
         Path(path).mkdir(parents=True, exist_ok=True)
 
-    checkpoint_dict = {}
-    # Model state dict
-    if isinstance(model, list):
-        # If a list of models is provided, add them all into a global dictionary
-        model_state_dict = {}
-        for i, model0 in enumerate(model):
-            model_state_dict[i] = model0.state_dict()
-        checkpoint_dict["model_state_dict"] = model_state_dict
-    elif model:
-        model_state_dict = model.state_dict()
-        checkpoint_dict["model_state_dict"] = model_state_dict
+    # == Saving model checkpoint ==
+    if models:
+        if not isinstance(models, list):
+            models = [models]
+        models = _unique_model_names(models)
+        for name, model in models.items():
+            # Get full file path / name
+            file_name = _get_checkpoint_filename(path, name, index=epoch, saving=True)
+            # Save state dictionary
+            if isinstance(model, modulus.Module):
+                model.save(file_name)
+            else:
+                torch.save(model.state_dict(), file_name)
+            checkpoint_logging.success(f"Saved model state dictionary: {file_name}")
 
+    # == Saving training checkpoint ==
+    checkpoint_dict = {}
     # Optimizer state dict
     if optimizer:
         checkpoint_dict["optimizer_state_dict"] = optimizer.state_dict()
@@ -166,21 +225,22 @@ def save_checkpoint(
         ] = _StaticCapture.scaler_singleton.state_dict()
 
     # Output file name
-    output_filename = _get_checkpoint_filename(path, epoch, True)
+    output_filename = _get_checkpoint_filename(path, index=epoch, saving=True)
     if epoch:
         checkpoint_dict["epoch"] = epoch
 
     # Save checkpoint to memory
-    checkpoint_logging.info(f"Saving checkpoint: {output_filename}")
-    torch.save(
-        checkpoint_dict,
-        output_filename,
-    )
+    if bool(checkpoint_dict):
+        torch.save(
+            checkpoint_dict,
+            output_filename,
+        )
+        checkpoint_logging.success(f"Saved training checkpoint: {output_filename}")
 
 
 def load_checkpoint(
     path: str,
-    model: Union[torch.nn.Module, List[torch.nn.Module], None] = None,
+    models: Union[torch.nn.Module, List[torch.nn.Module], None] = None,
     optimizer: Union[optimizer, None] = None,
     scheduler: Union[scheduler, None] = None,
     scaler: Union[scaler, None] = None,
@@ -189,15 +249,15 @@ def load_checkpoint(
 ) -> int:
     """Checkpoint loading utility
 
-    This loader is designed to be used with the save checkpoint utility in Modulus Launch.
-    Given a path, this method will try to find a checkpoint and load state dictionaries into
-    the provided training objects.
+    This loader is designed to be used with the save checkpoint utility in Modulus
+    Launch. Given a path, this method will try to find a checkpoint and load state
+    dictionaries into the provided training objects.
 
     Parameters
     ----------
     path : str
         Path to training checkpoint
-    model : Union[torch.nn.Module, List[torch.nn.Module], None], optional
+    models : Union[torch.nn.Module, List[torch.nn.Module], None], optional
         A single or list of PyTorch models, by default None
     optimizer : Union[optimizer, None], optional
         Optimizer, by default None
@@ -223,11 +283,34 @@ def load_checkpoint(
         )
         return 0
 
-    checkpoint_filename = _get_checkpoint_filename(path, epoch)
+    # == Loading model checkpoint ==
+    if models:
+        if not isinstance(models, list):
+            models = [models]
+        models = _unique_model_names(models)
+        for name, model in models.items():
+            # Get full file path / name
+            file_name = _get_checkpoint_filename(path, name, index=epoch)
+            if not Path(file_name).exists():
+                checkpoint_logging.error(
+                    f"Could not find valid model file {file_name}, skipping load"
+                )
+                continue
+            # Load state dictionary
+            if isinstance(model, modulus.Module):
+                model.load(file_name)
+            else:
+                model.load_state_dict(torch.load(file_name, map_location=device))
 
+            checkpoint_logging.success(
+                f"Loaded model state dictionary {file_name} to device {device}"
+            )
+
+    # == Loading training checkpoint ==
+    checkpoint_filename = _get_checkpoint_filename(path, index=epoch)
     if not Path(checkpoint_filename).is_file():
         checkpoint_logging.warning(
-            f"Could not find valid checkpoint file, skipping load"
+            "Could not find valid checkpoint file, skipping load"
         )
         return 0
 
@@ -235,17 +318,6 @@ def load_checkpoint(
     checkpoint_logging.success(
         f"Loaded checkpoint file {checkpoint_filename} to device {device}"
     )
-    # Model state dict
-    if "model_state_dict" in checkpoint_dict:
-        if isinstance(model, list):
-            # If a list of models is provided, add them all into a global dictionary
-            model_state_dict = checkpoint_dict["model_state_dict"]
-            for i, model0 in enumerate(model):
-                model0.load_state_dict(model_state_dict[i])
-                checkpoint_logging.success(f"Loaded model {i} state dictionary")
-        elif model:
-            model.load_state_dict(checkpoint_dict["model_state_dict"])
-            checkpoint_logging.success("Loaded model state dictionary")
 
     # Optimizer state dict
     if optimizer and "optimizer_state_dict" in checkpoint_dict:
