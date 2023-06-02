e0bad7bbcf9a06fa95ebcea05d506791d5769021
Noah D. Brenowitz <nbren12@gmail.com>
2023-06-01 17:46:50 -0700

    [other] Add modulus.models.fcn_mip_plugin (#44)
    * Add modulus.models.fcn_mip_plugin
    
    This also requires
    
    - filesystem abstraction layer
    - model metadata definition
    - model registry
    
    * Remove ModelRegistry
    
    This is not used anywhere or tested. It can be added again if needed.
    
    * Add license headers
    
    * Remove commented import
    
    * Remove plugin docstring
    
    * Remove schema
    
    * Fix fcn_mip_plugin test
    
    * Make CosZenWrapper private
    
    * Add graphcast fcn-mip plugin
    
    * make _fix_state_dict_keys private
    
    * Easy review comments
    
    * Remove unused filesytem utilities
    
    I've had several bugs and subsequent fixes related to glob, it's proved
    a tricky feature to handle correctly.
    
    * more simple fixes
    
    * Fix graphcast test
    
    * Use remote data in graphcast test
    
    * Add type hints
    
    * Add failing test
    
    * Fix cos zenith wrapper for batch_size=2
    
    * Remove get_model
    
    * Add fsspec
    
    * Make _download_cached private
    
    * Add docstring to graphcast_34ch
    
    * Remove unused import
    
    ---------
    
    Co-authored-by: Noah D. Brenowitz <nbrenowitz@nvidia.com>

---
+++ modulus/models/fcn_mip_plugin.py
@@ -0,0 +1,155 @@
+# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+import numpy as np
+import torch
+
+from modulus.models.sfno import sfnonet
+from modulus.utils import filesystem
+from modulus.utils.sfno.zenith_angle import cos_zenith_angle
+from modulus.utils.sfno.YParams import ParamsBase
+
+from modulus.models.graphcast.graph_cast_net import GraphCastNet
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class _DummyModule(torch.nn.Module):
+    """Hack to handle that checkpoint parameter names begin with "module." """
+
+    def __init__(self, model):
+        super().__init__()
+        self.module = model
+
+
+class _CosZenWrapper(torch.nn.Module):
+    def __init__(self, model, lon, lat):
+        super().__init__()
+        self.model = model
+        self.lon = lon
+        self.lat = lat
+
+    def forward(self, x, time):
+        lon_grid, lat_grid = np.meshgrid(self.lon, self.lat)
+        cosz = cos_zenith_angle(time, lon_grid, lat_grid)
+        cosz = cosz.astype(np.float32)
+        z = torch.from_numpy(cosz).to(device=x.device)
+        x, z = torch.broadcast_tensors(x, z)
+        x = torch.cat([x, z], dim=1)
+        return self.model(x)
+
+
+def sfno(package: filesystem.Package, pretrained: bool = True) -> torch.nn.Module:
+    """Load SFNO model from checkpoints trained with era5_wind"""
+    path = package.get("config.json")
+    params = ParamsBase.from_json(path)
+    model = sfnonet.SphericalFourierNeuralOperatorNet(params)
+    logger.info(str(params.to_dict()))
+
+    if pretrained:
+        weights = package.get("weights.tar")
+        checkpoint = torch.load(weights)
+        load_me = _DummyModule(model)
+        state = checkpoint["model_state"]
+        state = {"module.device_buffer": model.device_buffer, **state}
+        load_me.load_state_dict(state)
+
+    if params.add_zenith:
+        nlat = params.img_shape_x
+        nlon = params.img_shape_y
+        lat = 90 - np.arange(nlat) * 0.25
+        lon = np.arange(nlon) * 0.25
+        model = _CosZenWrapper(model, lon, lat)
+
+    return model
+
+
+class _GraphCastWrapper(torch.nn.Module):
+    def __init__(self, model, dtype):
+        super().__init__()
+        self.model = model
+        self.dtype = dtype
+
+    def forward(self, x):
+        x = x.to(self.dtype)
+        y = self.model(x)
+        return y
+
+
+def graphcast_34ch(
+    package: filesystem.Package, pretrained: bool = True
+) -> torch.nn.Module:
+    """Load Graphcast 34 channel model from a checkpoint"""
+    num_channels = 34
+
+    icospheres_path = package.get("icospheres.json")
+    static_data_path = package.get("static", recursive=True)
+
+    # instantiate the model, set dtype and move to device
+    base_model = (
+        GraphCastNet(
+            meshgraph_path=icospheres_path,
+            static_dataset_path=static_data_path,
+            input_dim_grid_nodes=num_channels,
+            input_dim_mesh_nodes=3,
+            input_dim_edges=4,
+            output_dim_grid_nodes=num_channels,
+            processor_layers=16,
+            hidden_dim=512,
+            do_concat_trick=True,
+        )
+        .to(dtype=torch.bfloat16)
+        .to(dist.device)
+    )
+
+    # set model to inference mode
+    base_model.eval()
+
+    model = _GraphCastWrapper(base_model, torch.bfloat16)
+
+    if pretrained:
+        path = package.get("weights.tar")
+        checkpoint = torch.load(path)
+        weights = checkpoint["model_state_dict"]
+        weights = _fix_state_dict_keys(weights, add_module=False)
+        model.model.load_state_dict(weights, strict=True)
+
+    return model
+
+
+def _fix_state_dict_keys(state_dict, add_module=False):
+    """Add or remove 'module.' from state_dict keys
+
+    Parameters
+    ----------
+    state_dict : Dict
+        Model state_dict
+    add_module : bool, optional
+        If True, will add 'module.' to keys, by default False
+
+    Returns
+    -------
+    Dict
+        Model state_dict with fixed keys
+    """
+    fixed_state_dict = {}
+    for key, value in state_dict.items():
+        if add_module:
+            new_key = "module." + key
+        else:
+            new_key = key.replace("module.", "")
+        fixed_state_dict[new_key] = value
+    return fixed_state_dict
