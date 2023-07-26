6c4fb118af261b87a3d7f16c64985f87a489575c
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2023-07-25 20:52:15 -0700

    Adding distributed afno implementation back in

--- modulus/models/afno/distributed/__init__.py
+++ modulus/models/afno/distributed/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# from .afno import DistributedAFNONet, DistributedAFNOArch
+from .afno import DistributedAFNONet, DistributedAFNOArch
