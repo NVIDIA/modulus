4070f61baef1e420b58dcaca31b747a590a1a491
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2023-07-31 15:43:40 -0700

    Fixing trunc_normal doctest
    Signed-off-by: Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>

--- modulus/models/afno/distributed/layers.py
+++ modulus/models/afno/distributed/layers.py
@@ -81,7 +81,7 @@ def trunc_normal_(tensor, mean=0.0, std=1.0, a=-2.0, b=2.0):
     b: the maximum cutoff value
     Examples:
     >>> w = torch.empty(3, 5)
-    >>> nn.init.trunc_normal_(w)
+    >>> o = nn.init.trunc_normal_(w)
     """
     return _no_grad_trunc_normal_(tensor, mean, std, a, b)
 
