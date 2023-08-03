ac050f480a57b207636fece81ee4290e0d6372ed
oliver <ohennigh@nvidia.com>
2023-08-03 11:27:37 -0700

    removed example external package

--- examples/ExternalPackage/example/load_model.py
+++
@@ -1,7 +0,0 @@
-from external_package.custom_model import CustomModel
-from modulus.models import Module
-
-model_from_library = CustomModel(16)
-model_from_modulus = Module.factory("CustomModel")(16)
-print(model_from_library)
-print(model_from_modulus)
