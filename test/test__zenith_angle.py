869d0d22d352ce44e68062403f42456bbbc25f1a
Noah D. Brenowitz <nbrenowitz@nvidia.com>
2023-10-02 23:38:31 +0000

    Increase precision of test

--- test/test__zenith_angle.py
+++ test/test__zenith_angle.py
@@ -35,13 +35,13 @@ from modulus.utils.sfno.zenith_angle import cos_zenith_angle
 @pytest.mark.parametrize(
     "time, lon, lat, expected",
     (
-        [datetime(2020, 3, 21, 12, 0, 0), 0.0, 0.0, 1.0],
-        [datetime(2020, 3, 21, 18, 0, 0), -90.0, 0.0, 1.0],
-        [datetime(2020, 3, 21, 18, 0, 0), 270.0, 0.0, 1.0],
-        [datetime(2020, 7, 6, 12, 0, 0), -90.0, 0.0, -0.0196310],
-        [datetime(2020, 7, 6, 9, 0, 0), 40.0, 40.0, 0.9501915],
-        [datetime(2020, 7, 6, 12, 0, 0), 0.0, 90.0, 0.3843733],
+        [datetime(2020, 3, 21, 12, 0, 0), 0.0, 0.0, 0.9994836252135212],
+        [datetime(2020, 3, 21, 18, 0, 0), -90.0, 0.0, 0.9994760971063111],
+        [datetime(2020, 3, 21, 18, 0, 0), 270.0, 0.0, 0.99947609879941],
+        [datetime(2020, 7, 6, 12, 0, 0), -90.0, 0.0, -0.019703903874316815],
+        [datetime(2020, 7, 6, 9, 0, 0), 40.0, 40.0, 0.9501802266240413],
+        [datetime(2020, 7, 6, 12, 0, 0), 0.0, 90.0, 0.3843918031907148],
     ),
 )
 def test__sun_zenith_angle(time, lon, lat, expected):
-    assert cos_zenith_angle(time, lon, lat) == pytest.approx(expected, abs=1e-3)
+    assert cos_zenith_angle(time, lon, lat) == pytest.approx(expected, abs=1e-9)
