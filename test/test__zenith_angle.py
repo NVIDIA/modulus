7f018823950c525679a5f80d51f9b104a719f59e
Noah D. Brenowitz <nbrenowitz@nvidia.com>
2023-10-02 23:31:35 +0000

    Add zenith angle tests

---
+++ test/test__zenith_angle.py
@@ -0,0 +1,47 @@
+# ignore_header_test
+
+# climt/LICENSE
+# @mcgibbon
+# BSD License
+# Copyright (c) 2016, Rodrigo Caballero
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice, this
+#   list of conditions and the following disclaimer in the documentation and/or
+#   other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+
+from datetime import datetime
+
+import pytest
+
+from modulus.utils.sfno.zenith_angle import cos_zenith_angle
+
+
+@pytest.mark.parametrize(
+    "time, lon, lat, expected",
+    (
+        [datetime(2020, 3, 21, 12, 0, 0), 0.0, 0.0, 1.0],
+        [datetime(2020, 3, 21, 18, 0, 0), -90.0, 0.0, 1.0],
+        [datetime(2020, 3, 21, 18, 0, 0), 270.0, 0.0, 1.0],
+        [datetime(2020, 7, 6, 12, 0, 0), -90.0, 0.0, -0.0196310],
+        [datetime(2020, 7, 6, 9, 0, 0), 40.0, 40.0, 0.9501915],
+        [datetime(2020, 7, 6, 12, 0, 0), 0.0, 90.0, 0.3843733],
+    ),
+)
+def test__sun_zenith_angle(time, lon, lat, expected):
+    assert cos_zenith_angle(time, lon, lat) == pytest.approx(expected, abs=1e-3)
