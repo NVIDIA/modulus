caa65ed99fb668874b123ced9cbf0df2e37cc4ae
Nick Geneva <nicholasgeneva@gmail.com>
2023-10-25 06:46:54 +0000

    ruff

--- test/models/test_from_checkpoint.py
+++ test/models/test_from_checkpoint.py
@@ -1,10 +1,28 @@
+# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+
 import pytest
 import torch
+
 import modulus
-from pathlib import Path
 
 
 class MockModel(modulus.Module):
+    """Fake model"""
+
     def __init__(self, layer_size=16):
         super().__init__()
         self.layer_size = layer_size
@@ -12,6 +30,8 @@ class MockModel(modulus.Module):
 
 
 class NewMockModel(modulus.Module):
+    """Fake model"""
+
     def __init__(self, layer_size=16):
         super().__init__()
         self.layer_size = layer_size
