f122efbf2e5f74067c0a4100d20adcf69c470abf
HanGao <hgao1@nd.edu>
2023-10-27 12:54:14 -0400

    minor

--- modulus/datapipes/gnn/vortex_shedding_re300_1000_dataset.py
+++ modulus/datapipes/gnn/vortex_shedding_re300_1000_dataset.py
@@ -1,10 +1,11 @@
 from dgl.data import DGLDataset
+from dgl.dataloading import GraphDataLoader
 import numpy as np
 import os
 import torch
 import dgl
 import pdb
-
+from utils import read_vtp_file, save_json, load_json
 
 
 
@@ -13,8 +14,9 @@ class VortexSheddingRe300To1000Dataset(DGLDataset):
 		self,
 		name="dataset",
 		data_dir='./',
+		split = 'train',
 		sequence_ids=[2],
-		verbose=False,
+		verbose=False
 	):
 		
 		super().__init__(
@@ -23,6 +25,7 @@ class VortexSheddingRe300To1000Dataset(DGLDataset):
 			)
 		self.data_dir = data_dir
 		self.sequence_ids = sequence_ids
+		self.split = split
 		self.rawData = np.load(os.path.join(self.data_dir,'rawData.npy'),
 							   allow_pickle = True)
 		
@@ -30,7 +33,7 @@ class VortexSheddingRe300To1000Dataset(DGLDataset):
 		self.solution_states = torch.from_numpy(self.rawData['x'][self.sequence_ids,:,:,:])
 
 		# cell volume
-		self.M = torch.from_numpy(self.rawData['mass'])
+		#self.M = torch.from_numpy(self.rawData['mass'])
 
 		# edge information
 		self.E = torch.from_numpy(self.rawData['edge_attr'])
@@ -43,7 +46,25 @@ class VortexSheddingRe300To1000Dataset(DGLDataset):
 		self.sequence_num = self.solution_states.shape[0]
 		self.num_nodes    = self.solution_states.shape[2]
 
-		
+		if self.split == "train":
+			self.edge_stats = self._get_edge_stats()
+		else:
+			self.edge_stats = load_json("edge_stats.json")
+			
+		if self.split == "train":
+			self.node_stats = self._get_node_stats()
+		else:
+			self.node_stats = load_json("node_stats.json")
+
+		# handle the normalization
+		for i in range(self.sequence_num):
+			for j in range(self.sequence_len):	
+				self.solution_states[i,j] = self.normalize(self.solution_states[i,j], 
+					                                       self.node_stats["node_mean"], 
+														   self.node_stats["node_std"])
+		self.E = self.normalize(self.E,
+			                    self.edge_stats["edge_mean"], 
+		 						self.edge_stats["edge_std"])
 	
 	def __len__(self):
 		return self.sequence_len * self.sequence_num
@@ -52,8 +73,9 @@ class VortexSheddingRe300To1000Dataset(DGLDataset):
 		sidx = idx // self.sequence_len
 		tidx = idx % self.sequence_len
 		
-		node_features = torch.cat([self.solution_states[sidx,tidx],
-								   self.M],dim=1)
+		# node_features = torch.cat([self.solution_states[sidx,tidx],
+		# 						   self.M],dim=1)
+		node_features = self.solution_states[sidx,tidx]
 		node_targets = self.solution_states[sidx,tidx]
 		graph = dgl.graph((self.A[0], self.A[1]), num_nodes=self.num_nodes)
 		graph.ndata["x"] = node_features
@@ -61,6 +83,47 @@ class VortexSheddingRe300To1000Dataset(DGLDataset):
 		graph.edata["a"] = self.E
 		return graph
 	
+
+	def _get_edge_stats(self):
+		stats = {
+			"edge_mean": self.E.mean(dim=0),
+			"edge_std": self.E.std(dim=0),
+		}
+		save_json(stats, "edge_stats.json")
+		return stats
+
+
+
+	def _get_node_stats(self):
+		stats = {
+			"node_mean": self.solution_states.mean(dim=[0,1,2]),
+			"node_std": self.solution_states.std(dim=[0,1,2]),
+		}
+		save_json(stats, "edge_stats.json")
+		return stats
+
+
+
+
+	@staticmethod
+	def normalize(invar, mu, std):
+		"""normalizes a tensor"""
+		assert invar.size()[-1] == mu.size()[-1]
+		assert invar.size()[-1] == std.size()[-1]
+		return (invar - mu.expand(invar.size())) / std.expand(invar.size())
+	
+	@staticmethod
+	def denormalize(invar, mu, std):
+		"""denormalizes a tensor"""
+		# assert invar.size()[-1] == mu.size()[-1]
+		# assert invar.size()[-1] == std.size()[-1]
+		denormalized_invar = invar * std + mu
+		return denormalized_invar
+	
 if __name__ == '__main__':
-	dloader = VortexSheddingRe300To1000Dataset()
-	pdb.set_trace()
+	data_set = VortexSheddingRe300To1000Dataset(split = 'train')
+	data_loader = GraphDataLoader(data_set,
+								  batch_size=5,
+								  shuffle=True)
+	for batched_graph in data_loader:
+		pdb.set_trace()
