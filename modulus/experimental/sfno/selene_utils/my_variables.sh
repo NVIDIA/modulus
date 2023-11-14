f483c5ab539812184af6bc673613b2544c458cae
Mohammad <mnabian@nvidia.com>
2023-11-14 13:19:55 -0800

    remove selene scripts & utils

--- modulus/experimental/sfno/selene_utils/my_variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export MY_SLURM_ACCOUNT="SLURM ACCOUNT NAME"
-export MY_WANDB_API_KEY="YOUR_API_KEY"
-export MY_OUTPUT_FOLDER="/lustre/fsw/nvresearch/jkossaifi/outputs/"
-export MY_PLOT_FOLDER="/lustre/fsw/nvresearch/jkossaifi/plots"
-export MY_CODE_FOLDER="/lustre/fsw/nvresearch/jkossaifi/era5_wind"
