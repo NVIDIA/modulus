471477eab52e9a4f6af3b11a6b775347ac9e6de9
Mohammad <mnabian@nvidia.com>
2023-11-14 17:58:37 -0800

    comment out sfno from fcn-mip plugin

--- modulus/models/fcn_mip_plugin.py
+++ modulus/models/fcn_mip_plugin.py
@@ -24,62 +24,60 @@ import xarray
 import modulus  # noqa: F401 for docs
 from modulus.models.dlwp import DLWP
 from modulus.models.graphcast.graph_cast_net import GraphCastNet
-from modulus.models.sfno import sfnonet
 from modulus.utils import filesystem
-from modulus.utils.sfno.YParams import ParamsBase
 from modulus.utils.zenith_angle import cos_zenith_angle
 
 logger = logging.getLogger(__name__)
 
 
-class _DummyModule(torch.nn.Module):
-    """Hack to handle that checkpoint parameter names begin with "module." """
+# class _DummyModule(torch.nn.Module):
+#     """Hack to handle that checkpoint parameter names begin with "module." """
 
-    def __init__(self, model):
-        super().__init__()
-        self.module = model
-
-
-class _CosZenWrapper(torch.nn.Module):
-    def __init__(self, model, lon, lat):
-        super().__init__()
-        self.model = model
-        self.lon = lon
-        self.lat = lat
-
-    def forward(self, x, time):
-        lon_grid, lat_grid = np.meshgrid(self.lon, self.lat)
-        cosz = cos_zenith_angle(time, lon_grid, lat_grid)
-        cosz = cosz.astype(np.float32)
-        z = torch.from_numpy(cosz).to(device=x.device)
-        x, z = torch.broadcast_tensors(x, z)
-        x = torch.cat([x, z], dim=1)
-        return self.model(x)
+#     def __init__(self, model):
+#         super().__init__()
+#         self.module = model
 
 
-def sfno(package: filesystem.Package, pretrained: bool = True) -> torch.nn.Module:
-    """Load SFNO model from checkpoints trained with era5_wind"""
-    path = package.get("config.json")
-    params = ParamsBase.from_json(path)
-    model = sfnonet.SphericalFourierNeuralOperatorNet(params)
-    logger.info(str(params.to_dict()))
+# class _CosZenWrapper(torch.nn.Module):
+#     def __init__(self, model, lon, lat):
+#         super().__init__()
+#         self.model = model
+#         self.lon = lon
+#         self.lat = lat
 
-    if pretrained:
-        weights = package.get("weights.tar")
-        checkpoint = torch.load(weights)
-        load_me = _DummyModule(model)
-        state = checkpoint["model_state"]
-        state = {"module.device_buffer": model.device_buffer, **state}
-        load_me.load_state_dict(state)
+#     def forward(self, x, time):
+#         lon_grid, lat_grid = np.meshgrid(self.lon, self.lat)
+#         cosz = cos_zenith_angle(time, lon_grid, lat_grid)
+#         cosz = cosz.astype(np.float32)
+#         z = torch.from_numpy(cosz).to(device=x.device)
+#         x, z = torch.broadcast_tensors(x, z)
+#         x = torch.cat([x, z], dim=1)
+#         return self.model(x)
 
-    if params.add_zenith:
-        nlat = params.img_shape_x
-        nlon = params.img_shape_y
-        lat = 90 - np.arange(nlat) * 0.25
-        lon = np.arange(nlon) * 0.25
-        model = _CosZenWrapper(model, lon, lat)
 
-    return model
+# def sfno(package: filesystem.Package, pretrained: bool = True) -> torch.nn.Module:
+#     """Load SFNO model from checkpoints trained with era5_wind"""
+#     path = package.get("config.json")
+#     params = ParamsBase.from_json(path)
+#     model = sfnonet.SphericalFourierNeuralOperatorNet(params)
+#     logger.info(str(params.to_dict()))
+
+#     if pretrained:
+#         weights = package.get("weights.tar")
+#         checkpoint = torch.load(weights)
+#         load_me = _DummyModule(model)
+#         state = checkpoint["model_state"]
+#         state = {"module.device_buffer": model.device_buffer, **state}
+#         load_me.load_state_dict(state)
+
+#     if params.add_zenith:
+#         nlat = params.img_shape_x
+#         nlon = params.img_shape_y
+#         lat = 90 - np.arange(nlat) * 0.25
+#         lon = np.arange(nlon) * 0.25
+#         model = _CosZenWrapper(model, lon, lat)
+
+#     return model
 
 
 class _GraphCastWrapper(torch.nn.Module):
