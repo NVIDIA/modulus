2602f4aeb57f8039a457ab84aac46dfbfe3eda26
Jussi Leinonen <jleinonen@nvidia.com>
2023-11-23 14:18:19 +0000

    Update datapipe with new code from diagnostic model repo

--- modulus/datapipes/climate/__init__.py
+++ modulus/datapipes/climate/__init__.py
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .climate import ClimateHDF5Datapipe
 from .era5_hdf5 import ERA5HDF5Datapipe
