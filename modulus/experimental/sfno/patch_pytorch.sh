4384262e3ae5cf56ae846c6d0c23cdd93b0ca6bb
Alexey Kamenev <alex.kamenev@gmail.com>
2023-11-27 14:20:10 -0800

    Add error checking, path and user handling.

--- modulus/experimental/sfno/patch_pytorch.sh
+++ modulus/experimental/sfno/patch_pytorch.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-cp third_party/torch/distributed/utils.py /usr/local/lib/python3.10/dist-packages/torch/distributed/
-echo "Patching complete"
+
+SRC_FILE=./third_party/torch/distributed/utils.py
+PYTHON_DIST_PKG_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
+
+if cp ${SRC_FILE} ${PYTHON_DIST_PKG_DIR}/torch/distributed/; then
+    echo "Patching complete"
+else
+    echo "Patching failed, possibly permissions issue, trying with sudo..."
+    if [ -n "$(which sudo)" ]; then
+        if sudo cp ${SRC_FILE} ${PYTHON_DIST_PKG_DIR}/torch/distributed/; then
+            echo "Patching complete"
+        else
+            echo "cp failed, giving up."
+        fi
+    else
+        echo "sudo was not found, giving up."
+    fi
+fi
