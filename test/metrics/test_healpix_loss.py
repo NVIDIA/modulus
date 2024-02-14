43fdccfdcbf5bdb48bfbd5356a69d1b8377e8951
David Pruitt <dpruitt@nvidia.com>
2024-02-14 18:24:05 +0000

    additional testing

--- test/metrics/test_healpix_loss.py
+++ test/metrics/test_healpix_loss.py
@@ -53,6 +53,7 @@ class trainer_helper:
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
 def test_BaseMSE(device, test_data, rtol: float = 1e-3, atol: float = 1e-3):
     mse_func = BaseMSE()
+    mse_func.setup(None) # for coverage
     channels, pred_tensor_np, targ_tensor_np = test_data()
 
     pred_tensor = torch.from_numpy(pred_tensor_np).to(device).expand(channels, -1, -1)
@@ -90,7 +91,6 @@ def test_BaseMSE(device, test_data, rtol: float = 1e-3, atol: float = 1e-3):
     # int( cos(x)^2 - cos(2x)^2 )dx, x = 0...2*pi = pi/4
     # So MSE should be pi/4 / (pi) = 0.25
     error = mse_func(pred_tensor**2, targ_tensor**2)
-    print(f"error {error}")
     assert torch.allclose(
         error,
         0.25 * torch.ones([1], dtype=torch.float32, device=device),
@@ -98,16 +98,37 @@ def test_BaseMSE(device, test_data, rtol: float = 1e-3, atol: float = 1e-3):
         atol=atol,
     )
 
+    # test for non averaged channesl
+    # make the last channel of prediction and target the same
+    tensor_size = pred_tensor.shape[-2:]
+    ones = torch.ones(tensor_size, device=device)
+
+    pred_tensor = pred_tensor.contiguous()
+    targ_tensor = targ_tensor.contiguous()
+    pred_tensor[0, 0, 0, -1, ...] = ones[...]
+    targ_tensor[0, 0, 0, -1, ...] = ones[...]
+    
+    error = mse_func(pred_tensor**2, targ_tensor**2, average_channels=False)
+    
+    expected_err = 0.25 * torch.ones(error.shape, dtype=torch.float32, device=device)
+    expected_err[-1] = 0
+
+    assert torch.allclose(
+        error,
+        expected_err,
+        rtol=rtol,
+        atol=atol,
+    )
+
+
+
+
 
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
 def test_WeightedMSE(device, test_data, rtol: float = 1e-3, atol: float = 1e-3):
     num_channels = 3
-    var_list = ["a", "b", "ones"]
     channels, pred_tensor_np, targ_tensor_np = test_data(channels=num_channels)
-    trainer = trainer_helper(
-        output_variables=var_list,
-        device=device,
-    )
+
 
     # first two channels will be as BaseMSE above, the last channel will be 0 loss
     # so per channel loss will be [0.25, 0.25, 0]
@@ -118,13 +139,28 @@ def test_WeightedMSE(device, test_data, rtol: float = 1e-3, atol: float = 1e-3):
     channel_weighted_mse = torch.Tensor([0.125, 0.0625, 0]).to(device)
     mean_weighted_mse = channel_weighted_mse.mean()
     weighted_mse_func = WeightedMSE(channel_weights)
+
+    trainer = trainer_helper(
+        output_variables=["a", "b", "ones"],
+        device=device,
+    )
     weighted_mse_func.setup(trainer)
+
+    # test setup fail case if number of variables doesn't match number of weights 
+    trainer = trainer_helper(
+        output_variables=["a", "b"],
+        device=device,
+    )
+    with pytest.raises(
+        ValueError, match="Length of outputs and loss_weights is not the same!"
+    ):
+        weighted_mse_func.setup(trainer)
 
     pred_tensor = torch.from_numpy(pred_tensor_np).to(device).expand(channels, -1, -1)
     targ_tensor = torch.from_numpy(targ_tensor_np).to(device).expand(channels, -1, -1)
 
     tensor_size = pred_tensor.shape[-2:]
-    ones = torch.zeros(tensor_size, device=device)
+    ones = torch.ones(tensor_size, device=device)
 
     # make the last channel of prediction and target the same
     pred_tensor = pred_tensor.contiguous()
@@ -155,9 +191,6 @@ def test_WeightedMSE(device, test_data, rtol: float = 1e-3, atol: float = 1e-3):
         )
 
     # test for 0 loss
-    print(
-        f"devices pred {pred_tensor.device} targ {targ_tensor.device} trainer {trainer.device}"
-    )
     error = weighted_mse_func(
         pred_tensor**2, targ_tensor**2, average_channels=False
     )
