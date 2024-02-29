7af53737dd74dcc7078b6071fb1bc327263cdbde
Mohammad Amin Nabian <m.a.nabiyan@gmail.com>
2024-02-28 17:35:21 -0800

    fix license test

--- modulus/datapipes/vfgn/reading_utils.py
+++ modulus/datapipes/vfgn/reading_utils.py
@@ -1,3 +1,6 @@
+# ignore_header_test
+# ruff: noqa: E402
+
 # © Copyright 2023 HP Development Company, L.P.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
