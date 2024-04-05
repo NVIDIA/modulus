fix scaling bug in deterministic sampler

modulus/utils/generative/sampler.py
@@ -185,7 +185,7 @@ def ablation_sampler(
         # Euler step.
         h = t_next - t_hat
         denoised = net(
-            x_hat / s(t_hat), x_lr / s(t_hat), sigma(t_hat), class_labels
+            x_hat / s(t_hat), x_lr, sigma(t_hat), class_labels
         ).to(torch.float64)
         d_cur = (
             sigma_deriv(t_hat) / sigma(t_hat) + s_deriv(t_hat) / s(t_hat)
@@ -198,7 +198,7 @@ def ablation_sampler(
             x_next = x_hat + h * d_cur
         else:
             denoised = net(
-                x_prime / s(t_prime), x_lr / s(t_prime), sigma(t_prime), class_labels
+                x_prime / s(t_prime), x_lr, sigma(t_prime), class_labels
             ).to(torch.float64)
             d_prime = (
                 sigma_deriv(t_prime) / sigma(t_prime) + s_deriv(t_prime) / s(t_prime)