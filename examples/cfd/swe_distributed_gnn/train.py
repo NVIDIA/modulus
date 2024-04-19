dd9f679fa7a03b8bb9ddff088d0cb83fde05ab87
Maximilian Stadler <mstadler@login-eos01.eos.clusters.nvidia.com>
2024-04-19 06:01:39 -0700

    update training script

--- examples/cfd/swe_distributed_gnn/train.py
+++ examples/cfd/swe_distributed_gnn/train.py
@@ -47,18 +47,6 @@ from pde_dataset import PdeDataset
 from modulus.distributed import DistributedManager, mark_module_as_shared
 from modulus.models.graphcast.graph_cast_net import GraphCastNet
 
-import wandb
-
-
-# control precision to a certain degree to
-# make sure that the loss pattern is easier
-# to sompare in slightly different accumulation
-USE_TF32 = False
-torch.backends.cuda.matmul.allow_tf32 = USE_TF32
-torch.backends.cudnn.allow_tf32 = USE_TF32
-torch.set_float32_matmul_precision("highest")
-torch.use_deterministic_algorithms(True, warn_only=True)
-
 
 def l2loss_sphere(solver, prd, tar, relative=False, squared=True):
     loss = solver.integrate_grid((prd - tar) ** 2, dimensionless=True).sum(dim=-1)
@@ -147,9 +135,10 @@ def autoregressive_inference(
                     plt.clf()
 
             nwp_times[iic] = time.time() - start_time
-
+            
             ref = dataset.solver.spec2grid(uspec)
             prd = prd * torch.sqrt(inp_var) + inp_mean
+
             losses[iic] = l2loss_sphere(dataset.solver, prd, ref, relative=True).item()
 
     return losses, fno_times, nwp_times
@@ -162,19 +151,25 @@ def train_model(
     optimizer,
     gscaler,
     dist_manager,
-    scheduler=None,
+    metrics,
     nepochs=10,
     nfuture=0,
     num_examples=512,
     num_valid=8,
     loss_fn="l2",
     enable_amp=False,
-    log_grads=0,
+    dummy_dataset=False,
 ):
     train_start = time.time()
 
     # count iterations
     iters = 0
+
+    metrics["current_lr"] = []
+    metrics["epoch_time"] = []
+    metrics["acc_loss"] = []
+    metrics["val_loss"] = []
+    metrics["epochs"] = []
 
     for epoch in range(nepochs):
 
@@ -211,42 +206,29 @@ def train_model(
         dataloader.dataset.set_initial_condition("random")
         dataloader.dataset.set_num_examples(num_valid)
 
-        # perform validation
-        valid_loss = 0
-        model.eval()
-        with torch.no_grad():
-            for inp, tar in dataloader:
-                prd = model(inp)
-                for _ in range(nfuture):
-                    prd = model(prd)
-                loss = l2loss_sphere(solver, prd, tar, relative=True)
-                valid_loss += loss.item() * inp.size(0)
+        # perform validation (only on actual data)
+        valid_loss = 0.0
+        if not dummy_dataset:
+            model.eval()
+            with torch.no_grad():
+                for inp, tar in dataloader:
+                    prd = model(inp)
+                    for _ in range(nfuture):
+                        prd = model(prd)
+                    loss = l2loss_sphere(solver, prd, tar, relative=True)
+                    valid_loss += loss.item() * inp.size(0)
 
-        valid_loss = valid_loss / len(dataloader.dataset)
-
-        if scheduler is not None:
-            scheduler.step(valid_loss)
+            valid_loss = valid_loss / len(dataloader.dataset)
 
         epoch_time = time.time() - epoch_start
 
-        if dist_manager.rank == 0:
-            print(
-                f"--------------------------------------------------------------------------------"
-            )
-            print(f"Epoch {epoch} summary:")
-            print(f"time taken: {epoch_time}")
-            print(f"accumulated training loss: {acc_loss}")
-            print(f"relative validation loss: {valid_loss}")
-
-            if wandb.run is not None:
+        if dist_manager.rank == 0 and metrics is not None:
                 current_lr = optimizer.param_groups[0]["lr"]
-                wandb.log(
-                    {
-                        "loss": acc_loss,
-                        "validation loss": valid_loss,
-                        "learning rate": current_lr,
-                    }
-                )
+                metrics["epochs"].append(epoch)
+                metrics["current_lr"].append(current_lr)
+                metrics["epoch_time"].append(epoch_time)
+                metrics["acc_loss"].append(acc_loss)
+                metrics["val_loss"].append(valid_loss)
 
     train_time = time.time() - train_start
 
@@ -256,8 +238,6 @@ def train_model(
         )
         print(f"done. Training took {train_time}.")
 
-    return valid_loss
-
 
 def main(
     mesh_size: int = 6,
@@ -266,14 +246,27 @@ def main(
     seed: int = 1234,
     root_path: Optional[str] = None,
 ):
+
+    # control precision to a certain degree to
+    # make sure that the loss pattern is easier
+    # to sompare in slightly different accumulation
+    if not dummy_dataset:
+        torch.backends.cuda.matmul.allow_tf32 = False
+        torch.backends.cudnn.allow_tf32 = False
+        torch.set_float32_matmul_precision("highest")
+
+    else:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+        torch.set_float32_matmul_precision("high")
+
     train = True
     load_checkpoint = False
     save_checkpoint = False
     enable_amp = False
-    log_grads = 0
 
-    nepochs = 10 if not short_run else 64
-    num_examples = 512 if not short_run else 1
+    nepochs = 10 if not short_run else 1
+    num_examples = 512 if not short_run else 64
     input_res = (256, 512)
 
     torch.manual_seed(seed)
@@ -286,20 +279,28 @@ def main(
     if DistributedManager().distributed:
         graph_partition_pg_name = "graph_partition"
         world_size = torch.distributed.get_world_size()
-        run_suffix = f"mg_{world_size}_mesh{mesh_size}_dummy{dummy_dataset}_seed{seed}"
+        run_suffix = f"mg-{world_size}_mesh-{mesh_size}_seed-{seed}"
+        if dummy_dataset:
+            run_suffix += "_dummy"
+        if short_run:
+            run_suffix += "_short"
         DistributedManager.create_process_subgroup(
             name=graph_partition_pg_name,
             size=world_size,
             verbose=True,
         )
     else:
-        run_suffix = f"sg_mesh{mesh_size}_dummy{dummy_dataset}_seed{seed}"
+        run_suffix = f"sg_mesh-{mesh_size}_seed-{seed}"
+        if dummy_dataset:
+            run_suffix += "_dummy"
+        if short_run:
+            run_suffix += "_short"
         graph_partition_pg_name = None
 
     dist_manager = DistributedManager()
 
     if dist_manager.rank == 0:
-        wandb.login()
+        print(f"starting to run {run_suffix} ...")
 
     model = GraphCastNet(
         meshgraph_path=f"./icospheres_{mesh_size}.json",
@@ -342,7 +343,7 @@ def main(
 
     if load_checkpoint:
         model.load_state_dict(
-            torch.load(os.path.join(root_path, "checkpoints", run_suffix, "model.pt"))
+            torch.load(os.path.join(root_path, run_suffix, "checkpoints", "model.pt"))
         )
 
     # 1 hour prediction steps
@@ -366,16 +367,8 @@ def main(
 
     # run the training
     if train:
-        if dist_manager.rank == 0:
-            run = wandb.init(
-                project="swe", group="SWE_GC", name="SWE_GC" + "_" + str(time.time())
-            )
-
         # optimizer:
         optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(
-            optimizer, milestones=[4, 8], gamma=0.5
-        )
         gscaler = amp.GradScaler(enabled=enable_amp)
 
         start_time = time.time()
@@ -389,65 +382,65 @@ def main(
             optimizer,
             gscaler,
             dist_manager,
-            scheduler=scheduler,
+            metrics,
             num_examples=num_examples,
             nepochs=nepochs,
             loss_fn="l2",
             enable_amp=enable_amp,
-            log_grads=log_grads,
+            dummy_dataset=dummy_dataset,
         )
 
         training_time = time.time() - start_time
 
         if dist_manager.rank == 0:
-            os.makedirs(os.path.join(root_path, "figures", run_suffix), exist_ok=True)
+            os.makedirs(os.path.join(root_path, run_suffix, "figures"), exist_ok=True)
             os.makedirs(
-                os.path.join(root_path, "output_data", run_suffix), exist_ok=True
+                os.path.join(root_path, run_suffix, "output_data"), exist_ok=True
             )
             if save_checkpoint:
                 os.makedirs(
-                    os.path.join(root_path, "checkpoints", run_suffix), exist_ok=True
+                    os.path.join(root_path, run_suffix, "checkpoints"), exist_ok=True
                 )
                 torch.save(
                     model.state_dict(),
-                    os.path.join(root_path, "checkpoints", run_suffix, "model.pt"),
+                    os.path.join(root_path, run_suffix, "checkpoints", "model.pt"),
                 )
 
     # set seed
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
 
-    with torch.inference_mode():
-        losses, fno_times, nwp_times = autoregressive_inference(
-            model,
-            dataset,
-            os.path.join(root_path, "figures", run_suffix),
-            dist_manager=dist_manager,
-            nsteps=nsteps,
-            autoreg_steps=10,
+    if not dummy_dataset:
+        with torch.inference_mode():
+            losses, fno_times, nwp_times = autoregressive_inference(
+                model,
+                dataset,
+                os.path.join(root_path, run_suffix, "figures"),
+                dist_manager=dist_manager,
+                nsteps=nsteps,
+                autoreg_steps=10,
+            )
+            metrics["loss_mean"] = np.mean(losses)
+            metrics["loss_std"] = np.std(losses)
+            metrics["fno_time_mean"] = np.mean(fno_times)
+            metrics["fno_time_std"] = np.std(fno_times)
+            metrics["nwp_time_mean"] = np.mean(nwp_times)
+            metrics["nwp_time_std"] = np.std(nwp_times)
+
+    if train:
+        metrics["training_time"] = training_time
+        metrics[f"max_memory_allocated_gib"] = (
+            torch.cuda.max_memory_allocated() * 1.0 / (1024**3)
         )
-        metrics["loss_mean"] = np.mean(losses)
-        metrics["loss_std"] = np.std(losses)
-        metrics["fno_time_mean"] = np.mean(fno_times)
-        metrics["fno_time_std"] = np.std(fno_times)
-        metrics["nwp_time_mean"] = np.mean(nwp_times)
-        metrics["nwp_time_std"] = np.std(nwp_times)
-        if train:
-            metrics["training_time"] = training_time
-            metrics[f"max_memory_allocated_gib"] = (
-                torch.cuda.max_memory_allocated() * 1.0 / (1024**3)
-            )
-            metrics[f"max_memory_reserved_gib"] = (
-                torch.cuda.max_memory_reserved() * 1.0 / (1024**3)
-            )
+        metrics[f"max_memory_reserved_gib"] = (
+            torch.cuda.max_memory_reserved() * 1.0 / (1024**3)
+        )
 
     if dist_manager.rank == 0:
         with open(
-            os.path.join(root_path, "output_data", run_suffix, "metrics.json"), "w"
+            os.path.join(root_path, run_suffix, "output_data", "metrics.json"), "w"
         ) as f:
             json.dump(metrics, f)
-        if train:
-            run.finish()
 
     DistributedManager.cleanup()
 
@@ -455,14 +448,14 @@ def main(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--mesh_size", type=int, default=6)
+    # random "dummy" data is useful for benchmark just
+    # the training part of things while actual data is used
+    # to verify numeric behavior
     parser.add_argument("--dummy_data", action="store_true")
     parser.add_argument("--short_run", action="store_true")
     parser.add_argument("--seed", type=int, default=1234)
     parser.add_argument("--root_path", type=str, default=None)
     args = parser.parse_args()
-
-    for k, v in vars(args).items():
-        print(f"{k}: {v}")
 
     main(
         mesh_size=args.mesh_size,
