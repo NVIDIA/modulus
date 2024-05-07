156be771bba7a63e8e1cfd24e7e5a70844d94cb9
XU HAN <47011438+XUHAN314@users.noreply.github.com>
2024-05-07 14:32:47 -0400

    GNN encoding/transformer (#310)
    * xu added gnn en/de-coder
    
    * add dataloader for parametric cylinder for Re num
    
    * minor
    
    * Xu Han added code
    
    * xu added code
    
    * minor
    
    * add launch
    
    * test for encoder
    
    * update test
    
    * split test
    
    * minor
    
    * validate train
    
    * Xu:test for sequence model
    
    * add notation,revise constants
    
    * add comments, remove unnecessary parameters
    
    * fix bugs
    
    * Xu:combine train and test class for sequence
    
    * merge
    
    * initial readme
    
    * minor
    
    * minor
    
    * fix readme
    
    * merge
    
    * fix bugs
    
    * support batch training for encoding
    
    * black formatting
    
    * linting & license headers
    
    * verify batch training
    
    * remanage the code
    
    * clean the code
    
    * fixing ci requirements
    
    * move the dataset folder
    
    ---------
    
    Co-authored-by: HanGao <hgao1@nd.edu>
    Co-authored-by: Mohammad Amin Nabian <m.a.nabiyan@gmail.com>
    Co-authored-by: Mohammad <mnabian@nvidia.com>
    Co-authored-by: root <root@eos0060.eos.clusters.nvidia.com>

---
+++ examples/cfd/vortex_shedding_mesh_reduced/README.md
@@ -0,0 +1,100 @@
+# Temporal attention model in Mesh-Reduced space for transient vortex shedding
+
+This example is an implementation of the paper "Predicting Physics in Mesh-reduced Space
+with Temporal Attention" in PyTorch.
+It demonstrates how to train a Graph Neural Network (GNN) as encoder to compress the
+high-dimensional
+physical state into latent space and apply a multi-head attention model for temporal
+predictions for
+the transient vortex shedding on parameterized geometries.
+
+## Problem overview
+
+## Dataset
+
+We use vortex shedding dataset for this example. The dataset includes
+51 training, and 50 test samples that are simulated using OpenFOAM
+with irregular triangle 2D meshes, each for 401 time steps with a time step size of
+0.5s. These samples vary in the Reynolds number. Each sample share the same mesh with
+1699 nodes.
+
+## Model overview and architecture
+
+The model is auto-regressive. It first encodes the graph state into a latent vector
+via a Graph
+Nueral Network. Then a multi-head temporal model takes the initial condition tokens
+and pysical paramerters
+as the input and predicts the solution for the following sequence in the latent space
+just like a language model.
+
+The model uses the input mesh to construct a bi-directional DGL graph for each sample.
+The node features include (3 in total):
+
+- Velocity components at time step $t$, i.e., $u_t$, $v_t$
+- Pressure at time step $t$, $p_t$
+
+The edge features for each sample are time-independent and include (3 in total):
+
+- Relative $x$ and $y$ distance between the two end nodes of an edge
+- L2 norm of the relative distance vector
+
+The output of the model is the velocity components for the following steps, i.e.,
+$[\ldots, (u_{t}$, $v_{t}), (u_{t+1}$, $v_{t+1}), \ldots]$, as well as the
+pressure $[\ldots,p_{t},p_{t+1}\,\ldots]$.
+
+For the PbGMR-GMUS, a hidden dimensionality of 128 is used in the encoder, and decoder.
+The encoder and decoder consist of two hidden layers. Batch size per GPU is set to 1
+for the encoding-decoding process.
+Mean aggregation is used in the processor for message aggregation. A learning rate of
+0.0001 is used, decaying
+exponentially with a rate of 0.9999991. Traing epochs is set as 300.
+
+For the multi-head attention temporal model, the dimension for each token is
+$3 \times 256 = 768$. The hidden dimension usded in
+the temporal model is $4 \times 768 = 4072$. The number of head is 8. Batch size
+per GPU is set to 10 for the sequence model training. Traing epochs is set as 200000.
+
+## Getting Started
+
+To download the data , run
+
+```bash
+cd dataset
+sh TODO: need a shell to download the raw dataset from a cloud storage.
+```
+
+This example requires the `torch-scatter` and  `torch-cluster` library for the
+graph nodes agrregation. Install with
+
+```bash
+conda install pytorch-scatter -c pyg
+conda install pytorch-cluster -c pyg
+```
+
+To train the encoding-decoding model, run
+
+```bash
+python train.py
+```
+
+To test the reconstruction error, run
+
+```bash
+python test.py
+```
+
+To train the sequence model, run
+
+```bash
+python train_sequence.py
+```
+
+Once the model is trained, run
+
+```bash
+python test_sequence.py
+```
+
+## Reference
+
+- [Predicting Physics in Mesh-reduced Space with Temporal Attention](https://arxiv.org/abs/2201.09113)
---
+++ examples/cfd/vortex_shedding_mesh_reduced/test_sequence.py
@@ -0,0 +1,114 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 - 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import time
+
+import numpy as np
+import torch
+import wandb as wb
+from torch.cuda.amp import GradScaler
+
+from constants import Constants
+from modulus.distributed.manager import DistributedManager
+from modulus.launch.logging import (
+    PythonLogger,
+    RankZeroLoggingWrapper,
+    initialize_wandb,
+)
+from modulus.launch.utils import load_checkpoint
+from modulus.models.mesh_reduced.mesh_reduced import Mesh_Reduced
+from train_sequence import Sequence_Trainer
+
+C = Constants()
+
+if __name__ == "__main__":
+    # initialize distributed manager
+    DistributedManager.initialize()
+    dist = DistributedManager()
+
+    # initialize loggers
+    logger = PythonLogger("main")  # General python logger
+    rank_zero_logger = RankZeroLoggingWrapper(logger, dist)  # Rank 0 logger
+    logger.file_logging()
+
+    position_mesh = torch.from_numpy(np.loadtxt(C.mesh_dir)).to(dist.device)
+    position_pivotal = torch.from_numpy(np.loadtxt(C.pivotal_dir)).to(dist.device)
+    # Load Graph Encoder
+    Encoder = Mesh_Reduced(
+        C.num_input_features, C.num_edge_features, C.num_output_features
+    )
+    Encoder = Encoder.to(dist.device)
+    _ = load_checkpoint(
+        os.path.join(C.ckpt_path, C.ckpt_name),
+        models=Encoder,
+        scaler=GradScaler(),
+        device=dist.device,
+    )
+
+    trainer = Sequence_Trainer(
+        wb,
+        dist,
+        produce_latents=False,
+        Encoder=Encoder,
+        position_mesh=position_mesh,
+        position_pivotal=position_pivotal,
+        rank_zero_logger=rank_zero_logger,
+    )
+    trainer.model.eval()
+    start = time.time()
+    rank_zero_logger.info("Testing started...")
+    for graph in trainer.dataloader_graph_test:
+        g = graph.to(dist.device)
+
+        break
+    ground_trueth = trainer.dataset_graph_test.solution_states
+
+    i = 0
+    relative_error_sum_u = 0
+    relative_error_sum_v = 0
+    relative_error_sum_p = 0
+
+    for lc in trainer.dataloader_test:
+        ground = ground_trueth[i].to(dist.device)
+
+        graph.ndata["x"]
+        samples, relative_error_u, relative_error_v, relative_error_p = trainer.sample(
+            lc[0][:, 0:2],
+            lc[1],
+            ground,
+            lc[0],
+            Encoder,
+            g,
+            position_mesh,
+            position_pivotal,
+        )
+        relative_error_sum_u = relative_error_sum_u + relative_error_u
+        relative_error_sum_v = relative_error_sum_v + relative_error_v
+        relative_error_sum_p = relative_error_sum_p + relative_error_p
+        i = i + 1
+    relative_error_mean_u = relative_error_sum_u / i
+    relative_error_mean_v = relative_error_sum_v / i
+    relative_error_mean_p = relative_error_sum_p / i
+
+    # avg_loss = loss_total/n_batch
+    rank_zero_logger.info(
+        f"relative_error_mean_u: {relative_error_mean_u:10.3e},relative_error_mean_v: {relative_error_mean_v:10.3e},relative_error_mean_p: {relative_error_mean_p:10.3e},\\\
+            time cost: {(time.time()-start):10.3e}"
+    )
+    # wb.log({"loss": loss.detach().cpu()})
+
+    rank_zero_logger.info("Sampling completed!")
