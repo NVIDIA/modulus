a29715b3c26b93d3b6d785ddf2f04e69c7abe59a
Stathi Fotiadis <sfotiadis@gmail.com>
2024-05-30 17:58:49 -0700

    Added EDMLossSR to __init__ imports

--- modulus/metrics/diffusion/__init__.py
+++ modulus/metrics/diffusion/__init__.py
@@ -15,4 +15,4 @@
 # limitations under the License.
 
 from .fid import calculate_fid_from_inception_stats
-from .loss import EDMLoss, RegressionLoss, ResLoss, VELoss, VELoss_dfsr, VPLoss
+from .loss import EDMLoss, EDMLossSR, RegressionLoss, ResLoss, VELoss, VELoss_dfsr, VPLoss
