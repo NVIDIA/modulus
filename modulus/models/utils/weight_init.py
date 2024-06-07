951f7926da6f52cbb68ea41b5c92fff5e7f641bc
Dallas Foster <dallasf@nvidia.com>
2024-06-06 19:01:51 -0700

    fix doctest

--- modulus/models/utils/weight_init.py
+++ modulus/models/utils/weight_init.py
@@ -70,16 +70,6 @@ def trunc_normal_(tensor, mean=0.0, std=1.0, a=-2.0, b=2.0):
     NOTE: this impl is similar to the PyTorch trunc_normal_, the bounds [a, b] are
     applied while sampling the normal with mean/std applied, therefore a, b args
     should be adjusted to match the range of mean, std args.
-
-    Args:
-        tensor: an n-dimensional `torch.Tensor`
-        mean: the mean of the normal distribution
-        std: the standard deviation of the normal distribution
-        a: the minimum cutoff value
-        b: the maximum cutoff value
-    Examples:
-        >>> w = torch.empty(3, 5)
-        >>> trunc_normal_(w)
     """
     with torch.no_grad():
         return _trunc_normal_(tensor, mean, std, a, b)
