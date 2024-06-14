3db953c31d41e6c7f1709cd71559086734c36e9a
root <root@eos0394.eos.clusters.nvidia.com>
2024-06-14 11:58:25 -0700

    area weighting for time diff

--- examples/weather/graphcast/compute_time_diff_std.py
+++ examples/weather/graphcast/compute_time_diff_std.py
@@ -24,6 +24,8 @@ from hydra.utils import to_absolute_path
 from omegaconf import DictConfig
 import numpy as np
 
+from loss.utils import grid_cell_area
+
 
 @hydra.main(version_base="1.3", config_path="conf", config_name="config")
 def main(cfg: DictConfig) -> None:
@@ -35,13 +37,14 @@ def main(cfg: DictConfig) -> None:
     # instantiate the training datapipe
     DataPipe = ERA5HDF5Datapipe  # [T,num_channel, 721, 1440], grid features
     datapipe = DataPipe(
-        data_dir=to_absolute_path(os.path.join("/data/era5_75var", "train")),
-        stats_dir=to_absolute_path(os.path.join("/data/era5_75var", "stats")),
-        channels=[i for i in range(73)],
-        num_samples_per_year=1408,
+        data_dir=to_absolute_path(os.path.join(cfg.dataset_path, "train")),
+        stats_dir=to_absolute_path(os.path.join(cfg.dataset_path, "stats")),
+        channels=[i for i in range(cfg.num_channels_climate)],
+        latlon_resolution=cfg.latlon_res,
+        num_samples_per_year=cfg.num_samples_per_year_train,
         num_steps=1,
         batch_size=1,
-        num_workers=8,
+        num_workers=cfg.num_workers,
         device=dist.device,
         process_rank=dist.rank,
         world_size=dist.world_size,
@@ -49,15 +52,22 @@ def main(cfg: DictConfig) -> None:
     )
     print(f"Loaded training datapipe of length {len(datapipe)}")
 
+    area = (
+        grid_cell_area(torch.linspace(-90, 90, steps=cfg.latlon_res[0]), unit="deg")
+        .unsqueeze(1)
+        .to(dist.device)
+    )
+
     mean, mean_sqr = 0, 0
     for i, data in enumerate(datapipe):
         invar = data[0]["invar"]
         outvar = data[0]["outvar"][0]
         diff = outvar - invar
-        diff_sqr = torch.square(diff)
+        weighted_diff = area * diff
+        weighted_diff_sqr = torch.square(weighted_diff)
 
-        mean += torch.mean(diff, dim=(2, 3)) / len(datapipe)
-        mean_sqr += torch.mean(diff_sqr, dim=(2, 3)) / len(datapipe)
+        mean += torch.mean(weighted_diff, dim=(2, 3)) / len(datapipe)
+        mean_sqr += torch.mean(weighted_diff_sqr, dim=(2, 3)) / len(datapipe)
 
         if i % 100 == 0 and i != 0 and dist.rank == 0:
             print("Number of iterations %d" % i)
