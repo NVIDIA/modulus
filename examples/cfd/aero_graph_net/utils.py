1b81fc77fcb1a0055595054924649c00f0ab4aee
Kaustubh <ktangsali@nvidia.com>
2024-06-17 23:43:29 +0000

    add inference scripts

--- examples/cfd/aero_graph_net/utils.py
+++ examples/cfd/aero_graph_net/utils.py
@@ -18,6 +18,9 @@ from typing import Any, Mapping, Optional
 
 import torch
 from torch import Tensor
+import numpy as np
+import pyvista as pv
+import shapely
 
 from dgl import DGLGraph
 
@@ -31,32 +34,91 @@ class RRMSELoss(torch.nn.Module):
         ).mean()
 
 
-def compute_drag_coefficient(normals, area, coeff, p, s):
+def compute_frontal_area(mesh: pv.PolyData, direction: str = "x"):
+    """
+    Compute frontal area of a given mesh
+    Ref: https://github.com/pyvista/pyvista/discussions/5211#discussioncomment-7794449
+
+    Parameters:
+    -----------
+    mesh: pv.PolyData
+        Input mesh
+    direction: str, optional
+        Direction to project the area. Defaults to "x".
+
+    Raises:
+    -------
+    ValueError: Only supports x, y and z projection for computing frontal area.
+
+    Returns:
+    --------
+    frontal area: float
+        Frontal area of the mesh in the given direction
+    """
+    direction_map = {
+        "x": ((1, 0, 0), [1, 2]),
+        "y": ((0, 1, 0), [0, 2]),
+        "z": ((0, 0, 1), [0, 1]),
+    }
+
+    if direction not in direction_map:
+        raise ValueError("Direction must be x, y or z only")
+
+    normal, indices = direction_map[direction]
+    areas_proj = mesh.project_points_to_plane(origin=(0, 0, 0), normal=normal)
+    merged = shapely.union_all(
+        [
+            shapely.Polygon(
+                np.stack(
+                    [
+                        areas_proj.points[tri, indices[0]],
+                        areas_proj.points[tri, indices[1]],
+                    ],
+                    axis=1,
+                )
+            )
+            for tri in areas_proj.triangulate().regular_faces
+        ]
+    )
+
+    return merged.area
+
+
+def compute_drag_coefficient(
+    normals: np.ndarray,
+    area: np.ndarray,
+    coeff: float,
+    p: np.ndarray,
+    wss: np.ndarray,
+    drag_direction: np.ndarray = np.array([1, 0, 0]),
+):
     """
     Compute drag coefficient for a given mesh.
 
     Parameters:
     -----------
-    normals: Tensor
-        The surface normals mapped onto nodes
-    area: Tensor
-        The surface areas of each cell mapped onto nodes
-    coeff: Tensor
-        Dynamic pressure times the frontal area
-    p: Tensor
-        Pressure distribution on the mesh
-    s: Tensor
-        Wall shear stress distribution on the mesh
+    normals: np.ndarray
+        The surface normals on cells of the mesh
+    area: np.ndarray
+        The surface areas of each cell
+    coeff: float
+        Reciprocal of dynamic pressure times the frontal area, i.e. 2/(A * rho * U^2)
+    p: np.ndarray
+        Pressure distribution on the mesh (on each cell)
+    s: np.ndarray
+        Wall shear stress distribution on the mesh (on each cell)
+    drag_direction: np.ndarray
+        Direction of drag, default is np.array([1, 0, 0])
 
     Returns:
     --------
-    c_drag: float:
+    c_drag: float
         Computed drag coefficient
     """
 
     # Compute coefficients
-    c_p = coeff * torch.dot(normals[:, 0], area * p)
-    c_f = -coeff * torch.dot(s[:, 0], area)
+    c_p = coeff * np.sum(np.dot(normals, drag_direction) * area * p)
+    c_f = -coeff * np.sum(np.dot(wss, drag_direction) * area)
 
     # Compute total drag coefficients
     c_drag = c_p + c_f
