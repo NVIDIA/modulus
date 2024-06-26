7a631ac01f80ba7bb70d8dee49c92ac234e624e9
Kaustubh Tangsali <71059996+ktangsali@users.noreply.github.com>
2024-06-26 14:26:16 -0700

    Add inference scripts for Ahmed body (#556)
    * add inference scripts
    
    * move the inference notebook
    
    * generalize the force coeff calculation util
    
    * update docs
    
    * return cp and cf separately

--- examples/cfd/aero_graph_net/utils.py
+++ examples/cfd/aero_graph_net/utils.py
@@ -18,6 +18,9 @@ from typing import Any, Mapping, Optional
 
 import torch
 from torch import Tensor
+import numpy as np
+import pyvista as pv
+import shapely
 
 from dgl import DGLGraph
 
@@ -31,37 +34,105 @@ class RRMSELoss(torch.nn.Module):
         ).mean()
 
 
-def compute_drag_coefficient(normals, area, coeff, p, s):
+def compute_frontal_area(mesh: pv.PolyData, direction: str = "x"):
     """
-    Compute drag coefficient for a given mesh.
+    Compute frontal area of a given mesh
+    Ref: https://github.com/pyvista/pyvista/discussions/5211#discussioncomment-7794449
 
     Parameters:
     -----------
-    normals: Tensor
-        The surface normals mapped onto nodes
-    area: Tensor
-        The surface areas of each cell mapped onto nodes
-    coeff: Tensor
-        Dynamic pressure times the frontal area
-    p: Tensor
-        Pressure distribution on the mesh
-    s: Tensor
-        Wall shear stress distribution on the mesh
+    mesh: pv.PolyData
+        Input mesh
+    direction: str, optional
+        Direction to project the area. Defaults to "x".
+
+    Raises:
+    -------
+    ValueError: Only supports x, y and z projection for computing frontal area.
 
     Returns:
     --------
-    c_drag: float:
-        Computed drag coefficient
+    frontal area: float
+        Frontal area of the mesh in the given direction
+    """
+    direction_map = {
+        "x": ((1, 0, 0), [1, 2]),
+        "y": ((0, 1, 0), [0, 2]),
+        "z": ((0, 0, 1), [0, 1]),
+    }
+
+    if direction not in direction_map:
+        raise ValueError("Direction must be x, y or z only")
+
+    normal, indices = direction_map[direction]
+    areas_proj = mesh.project_points_to_plane(origin=(0, 0, 0), normal=normal)
+    merged = shapely.union_all(
+        [
+            shapely.Polygon(
+                np.stack(
+                    [
+                        areas_proj.points[tri, indices[0]],
+                        areas_proj.points[tri, indices[1]],
+                    ],
+                    axis=1,
+                )
+            )
+            for tri in areas_proj.triangulate().regular_faces
+        ]
+    )
+
+    return merged.area
+
+
+def compute_force_coefficients(
+    normals: np.ndarray,
+    area: np.ndarray,
+    coeff: float,
+    p: np.ndarray,
+    wss: np.ndarray,
+    force_direction: np.ndarray = np.array([1, 0, 0]),
+):
+    """
+    Computes force coefficients for a given mesh. Output includes the pressure and skin
+    friction components. Can be used to compute lift and drag.
+    For drag, use the `force_direction` as the direction of the motion,
+    e.g. [1, 0, 0] for flow in x direction.
+    For lift, use the `force_direction` as the direction perpendicular to the motion,
+    e.g. [0, 1, 0] for flow in x direction and weight in y direction.
+
+    Parameters:
+    -----------
+    normals: np.ndarray
+        The surface normals on cells of the mesh
+    area: np.ndarray
+        The surface areas of each cell
+    coeff: float
+        Reciprocal of dynamic pressure times the frontal area, i.e. 2/(A * rho * U^2)
+    p: np.ndarray
+        Pressure distribution on the mesh (on each cell)
+    wss: np.ndarray
+        Wall shear stress distribution on the mesh (on each cell)
+    force_direction: np.ndarray
+        Direction to compute the force, default is np.array([1, 0, 0])
+
+    Returns:
+    --------
+    c_total: float
+        Computed total force coefficient
+    c_p: float
+        Computed pressure force coefficient
+    c_f: float
+        Computed skin friction coefficient
     """
 
     # Compute coefficients
-    c_p = coeff * torch.dot(normals[:, 0], area * p)
-    c_f = -coeff * torch.dot(s[:, 0], area)
+    c_p = coeff * np.sum(np.dot(normals, force_direction) * area * p)
+    c_f = -coeff * np.sum(np.dot(wss, force_direction) * area)
 
-    # Compute total drag coefficients
-    c_drag = c_p + c_f
+    # Compute total force coefficients
+    c_total = c_p + c_f
 
-    return c_drag
+    return c_total, c_p, c_f
 
 
 def batch_as_dict(
@@ -79,3 +150,26 @@ def batch_as_dict(
         k: v.to(device) if isinstance(v, (Tensor, DGLGraph)) else v
         for k, v in batch.items()
     }
+
+
+def relative_lp_error(pred, y, p=2):
+    """
+    Calculate relative L2 error norm
+    Parameters:
+    -----------
+    pred: torch.Tensor
+        Prediction
+    y: torch.Tensor
+        Ground truth
+    Returns:
+    --------
+    error: float
+        Calculated relative L2 error norm (percentage) on cpu
+    """
+
+    error = (
+        torch.mean(torch.linalg.norm(pred - y, ord=p) / torch.linalg.norm(y, ord=p))
+        .cpu()
+        .numpy()
+    )
+    return error * 100
