9050d3e30bd5988e8dd271f012f5aba5c127f41f
Dallas Foster <dallasf@nvidia.com>
2024-07-11 11:23:06 -0700

    add regression and finite ensemble accuracy test

--- test/metrics/test_metrics_general.py
+++ test/metrics/test_metrics_general.py
@@ -287,6 +287,71 @@ def test_fair_crps_dim_arg_works(device):
 
 
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
+@pytest.mark.parametrize("num", [10, 23, 59])
+@pytest.mark.parametrize("biased", [True, False])
+def test_crps_finite(device, num, biased):
+
+    # Test biased and unbiased on uniform data with analytic result
+    pred = torch.linspace(0, 1, num + 1).unsqueeze(-1).to(device)
+    obs = torch.zeros([1], device=device)
+
+    analytic = (2 * num + 1) / (6 * num + 6) if biased else (num - 1) / (3 * num)
+    analytic = torch.as_tensor([analytic], device=device)
+
+    assert torch.all(torch.isclose(analytic, crps.kcrps(pred, obs, biased=biased)))
+
+    # Test biased on random data
+    pred = torch.as_tensor(
+        [
+            -1.9293,
+            0.6454,
+            -0.3902,
+            -1.0438,
+            -1.3573,
+            -0.2942,
+            2.6269,
+            1.0405,
+            -0.5659,
+            -0.1438,
+            -0.3993,
+            0.7306,
+            -0.8229,
+            -0.8500,
+            -0.5732,
+            0.6746,
+            0.7208,
+            0.6172,
+            -1.6648,
+            0.5183,
+            -0.4850,
+            0.3033,
+            2.4232,
+            0.4714,
+            -0.6040,
+            -0.4617,
+            1.3324,
+            -0.7937,
+            0.8862,
+            -1.2291,
+            2.7559,
+            -1.0750,
+            -0.3882,
+            0.2331,
+            0.4886,
+            -0.3715,
+            -0.3438,
+            -0.4229,
+            0.7913,
+            1.0469,
+        ],
+        device=device,
+    ).reshape([-1, 1])
+    obs = torch.as_tensor([-1.1569], device=device)
+    analytic = torch.as_tensor([0.7027], device=device)
+    assert torch.all(torch.isclose(analytic, crps.kcrps(pred, obs, biased=True)))
+
+
+@pytest.mark.parametrize("device", ["cuda:0", "cpu"])
 def test_crps(device, rtol: float = 1e-3, atol: float = 1e-3):
     # Uses eq (5) from Gneiting et al. https://doi.org/10.1175/MWR2904.1
     # crps(N(0, 1), 0.0) = 2 / sqrt(2*pi) - 1/sqrt(pi) ~= 0.23...
