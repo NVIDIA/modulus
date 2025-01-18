6d1bacbcd61142539c2578e07bc4a2817418f5ca
Chen <lei.chen1@hp.com>
2025-01-17 18:03:21 -0800

    clean up inference file
    Signed-off-by: Chen <lei.chen1@hp.com>

--- examples/additive_manufacturing/compensation/inference.py
+++ examples/additive_manufacturing/compensation/inference.py
@@ -13,7 +13,6 @@ from hydra import compose, initialize
 from utils import log_string, tic, toc
 
 from modulus.models.dgcnn.dgcnn_compensation import DGCNN
-
 # from modulus.models.dgcnn.dgcnn_compensation import DGCNN_ocardo
 
 
@@ -39,17 +38,19 @@ def main():
     np.random.seed(cfg.inference_options.seed)
 
     # model setting
-    device = torch.device("cpu")  # device('cuda' if args.cuda else 'cpu')
-    model = DGCNN().to(device)
+    device = torch.device("cuda" if cfg.general.cuda else "cpu")
 
-    model.load_state_dict(
+    # Initialize compensator
+    compensator = DGCNN().to(device)
+    compensator.load_state_dict(
         torch.load(cfg.inference_options.generator_path, map_location="cpu"),
         strict=False,
     )
-    for g in model.parameters():
+    for g in compensator.parameters():
         g.requires_grad = False  # to avoid computation
     log_string(LOG_FOUT, "Trained Shape Compensation model loaded")
 
+    # Initialize predictor
     discriminator = DGCNN().to(device)
     discriminator.load_state_dict(
         torch.load(cfg.inference_options.discriminator_path, map_location="cpu"),
@@ -62,26 +63,26 @@ def main():
     save_path = cfg.inference_options.save_path
     os.makedirs(save_path, exist_ok=True)
 
-    # tic()
+    # Load parts data, in STL or OBJ format
+    # todo: change the path to search all subfolders
     data_path = cfg.inference_options.data_path
-    models = glob.glob(data_path + "/*.stl") + glob.glob(
+    parts_ds = glob.glob(data_path + "/*.stl") + glob.glob(
         data_path + "/*.obj"
     )  # main processing
-    log_string(LOG_FOUT, f"Start processing data .. {models}")
+    log_string(LOG_FOUT, f"\n\nStart processing data .. {parts_ds}")
 
-    for m in models:
+    for part_ in parts_ds:
         tic()
-        log_string(LOG_FOUT, f"processing Part {m}")
+        log_string(LOG_FOUT, f"Processing Part {part_}")
 
         # load data as mesh
-        cad = trimesh.load_mesh(m)
+        cad = trimesh.load_mesh(part_)
         # convert mesh to points by taking vertices
         pts1 = torch.FloatTensor(np.asarray(cad.vertices))
 
         # randomize point indices
         rand = np.arange(len(pts1))
         np.random.shuffle(rand)
-        # print(rand)
         pts1_full = pts1[rand]
         log_string(LOG_FOUT, f"Part full shape: {pts1_full.shape}")
 
@@ -91,47 +92,41 @@ def main():
         comp_res = []
         out_res = []
         for i in range(n + 1):
-            print("batching %03d" % i)
+            log_string(LOG_FOUT, f"batching {i}")
             if i == n and len(pts1_full) % subsample == 0:
-                print("no need to process")
+                log_string(LOG_FOUT, "no need to process")
                 continue
             elif i == n:
                 pts1 = pts1_full[-subsample:]
             else:
                 pts1 = pts1_full[subsample * i : subsample * (i + 1)]
-            if cfg.cuda:
+
+            edge_index = torch_geometric.nn.knn_graph(pts1, 20)
+            if cfg.general.cuda:
+                log_string(LOG_FOUT, "Use Cuda")
                 pts1 = pts1.squeeze(0).to(device).squeeze(0)
-                edge_index = torch_geometric.nn.knn_graph(pts1, 20)
                 edge_index = edge_index.squeeze().to(device)
 
             # Compensate the original CAD, then predict the deviation from the compensated CAD
-            com = model(torch_geometric.data.Data(x=pts1, edge_index=edge_index))
+            com = compensator(torch_geometric.data.Data(x=pts1, edge_index=edge_index))
             out = discriminator(torch_geometric.data.Data(x=com, edge_index=edge_index))
 
             # Directly predict the deviation from the original CAD
             pre = discriminator(
                 torch_geometric.data.Data(x=pts1, edge_index=edge_index)
             )
-            com = com.detach().cpu()  # + m
-            out = out.detach().cpu()  # + m
-            pre = pre.detach().cpu()  # + m
 
             if i == n:
                 valid = len(pts1_full) - n * subsample
                 com = com[-valid:]
                 out = out[-valid:]
                 pre = pre[-valid:]
-                # print(com.shape)
-                # print(pre.shape)
-                # print(out.shape)
-                # print(valid)
 
-            cad_comp_pts = com
-            cad_pred_pts = pre
-            cad_outp_pts = out
-            comp_res.append(cad_comp_pts.numpy())
-            pred_res.append(cad_pred_pts.numpy())
-            out_res.append(cad_outp_pts.numpy())
+            comp_res.append(com.detach().cpu().numpy())
+            pred_res.append(pre.detach().cpu().numpy())
+            out_res.append(out.detach().cpu().numpy())
+
+        # Concatenate batches data to the final full part
         cad_comp_pts = np.concatenate(comp_res, 0)
         cad_pred_pts = np.concatenate(pred_res, 0)
         cad_outp_pts = np.concatenate(out_res, 0)
@@ -147,53 +142,51 @@ def main():
             cad_comp_res[rand[i]] = cad_comp_pts[i]
             cad_pred_res[rand[i]] = cad_pred_pts[i]
             cad_outp_res[rand[i]] = cad_outp_pts[i]
-        print(pts1)
-        print(pts1_res)
-        print(cad_comp_pts.shape)
 
         # Get input STL model name
-        modelname = os.path.basename(m)
-        print("modelname: " + modelname)
-        subfolder = save_path + modelname[:-4]  # + "_compensated/"
-        print("subfolder: ", subfolder)
+        part_name = os.path.basename(part_)
+        subfolder = save_path + part_name[:-4]  # + "_compensated/"
 
         if not os.path.exists(subfolder):
             os.mkdir(subfolder)
 
         np.savetxt(
-            os.path.join(subfolder, modelname[:-4] + "_cad.csv"),
+            os.path.join(subfolder, part_name[:-4] + "_cad.csv"),
             pts1_full.cpu().numpy(),
             fmt="%.8f",
             delimiter=",",
         )
+        log_string(LOG_FOUT, "Wrote to CAD mesh")
+
         np.savetxt(
-            os.path.join(subfolder, modelname[:-4] + "_comp.csv"),
+            os.path.join(subfolder, part_name[:-4] + "_comp.csv"),
             cad_comp_res,
             fmt="%.8f",
             delimiter=",",
         )
+        log_string(LOG_FOUT, f"Wrote to Compensated pointcloud: {part_name[:-4]}_comp.csv")
 
         cad.vertices = cad_comp_res
         cad.export(
-            os.path.join(subfolder, modelname[:-4] + "_comp.stl")
-        )  # = trimesh.load_mesh(args.data_path)
+            os.path.join(subfolder, part_name[:-4] + "_comp.stl")
+        )
+        log_string(LOG_FOUT, f"Wrote to Compensated mesh: {part_name[:-4]}_comp.stl")
 
         if cfg.inference_options.save_extra:
-            print("Saving extra files...")
-            print(cad_pred_pts.shape)
-            print(cad_outp_pts.shape)
+            log_string(LOG_FOUT, "Saving extra files...")
             np.savetxt(
-                os.path.join(subfolder, modelname[:-4] + "_pred.csv"),
+                os.path.join(subfolder, part_name[:-4] + "_pred.csv"),
                 cad_pred_res,
                 fmt="%.8f",
                 delimiter=",",
             )
             np.savetxt(
-                os.path.join(subfolder, modelname[:-4] + "_outp.csv"),
+                os.path.join(subfolder, part_name[:-4] + "_outp.csv"),
                 cad_outp_res,
                 fmt="%.8f",
                 delimiter=",",
             )
+        log_string(LOG_FOUT, "Wrote to deviation from the original CAD, and deviation from the compensated CAD")
 
         toc()
 
@@ -206,4 +199,5 @@ if __name__ == "__main__":
         distributed_option = cfg.general.use_distributed
         device = torch.device("cuda" if cfg.general.cuda else "cpu")
 
+    os.makedirs(cfg.inference_options.save_path, exist_ok=True)
     main()
